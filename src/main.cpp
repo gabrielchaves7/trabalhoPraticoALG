1eebe11eebc4269048efc35b99a6aa02386595f8
Gabriel Chaves <gabrielbum@gmail.com>
2021-01-07 14:23:42 -0300

    começo da implementação para realizar o cálculo de logística

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,57 @@
 #include <vector>
 #include <string>
 #include <sstream>
-#include <list> 
-using namespace std; 
+#include <list>
+using namespace std;
+const int temperaturaInicial = -90;
+const int limiteTemperatura = -60;
 
-void leEntradaCds(int quantidadeCentrosDeDistribuicao, vector<vector<int>> &arrayCD){
+void leEntradaCds(int quantidadeCentrosDeDistribuicao, vector<int> &arrayCD)
+{
   string line;
   for (int i = 0; i < quantidadeCentrosDeDistribuicao; i++)
   {
     getline(cin, line);
     istringstream iss(line);
-    vector<int> temp;
-    for(string s; iss >> s; )
-      temp.push_back(stoi(s));
-    arrayCD.push_back(temp);
+    for (string s; iss >> s;)
+    {
+      arrayCD.push_back(stoi(s));
+    }
   }
+}
+
+void leEntradaPvs(int quantidadePontosDeVacinacao, vector<vector<int>> &arrayPV)
+{
+  string line;
+  for (int i = 0; i < quantidadePontosDeVacinacao; i++)
+  {
+    getline(cin, line);
+    istringstream iss(line);
+    vector<int> caminhosPV;
+    for (string s; iss >> s;)
+      caminhosPV.push_back(stoi(s));
+    arrayPV.push_back(caminhosPV);
+  }
+}
+
+void realizaLogistica(int inicio, vector<int> &listaPvsAlcancados, vector<vector<int>> arrayPV, int temperaturaAtual, int perdaTemperatura)
+{
+  temperaturaAtual = temperaturaAtual + perdaTemperatura;
+  if (temperaturaAtual >= limiteTemperatura)
+    return;
+
+  int qtdPvsJaAlcancados = listaPvsAlcancados.size();
+  for (int i = inicio; i < qtdPvsJaAlcancados; i++)
+  {
+    int pvAtual = listaPvsAlcancados.at(i);
+    int quantidadePvsAlcancaveis = arrayPV[pvAtual - 1].size();
+    for (int k = 0; k < quantidadePvsAlcancaveis; k++)
+    {
+      listaPvsAlcancados.push_back(arrayPV[pvAtual - 1].at(k));
+    }
+  }
+
+  return realizaLogistica(qtdPvsJaAlcancados, listaPvsAlcancados, arrayPV, temperaturaAtual, perdaTemperatura);
 }
 
 int main()
@@ -26,21 +63,27 @@ int main()
 
   string temp;
   getline(cin, temp);
-  vector<string> result; 
+  vector<string> result;
   istringstream iss(temp);
-  for(string s; iss >> s; ) 
-      result.push_back(s);
+  for (string s; iss >> s;)
+    result.push_back(s);
 
   quantidadeCentrosDeDistribuicao = stoi(result.at(0));
   quantidadePontosDeVacinacao = stoi(result.at(1));
   perdaTemperatura = stoi(result.at(2));
 
-  vector<vector<int>> arrayCD;
-  leEntradaCds(quantidadeCentrosDeDistribuicao, arrayCD);
-  for (unsigned i=0; i<arrayCD.size(); i++){
-    int teste = arrayCD.at(0).at(i);
+  vector<int> listaPvsAlcancados;
+  vector<vector<int>> arrayPV;
+  leEntradaCds(quantidadeCentrosDeDistribuicao, listaPvsAlcancados);
+  leEntradaPvs(quantidadePontosDeVacinacao, arrayPV);
+  realizaLogistica(0, listaPvsAlcancados, arrayPV, temperaturaInicial, perdaTemperatura);
+
+  for (unsigned i = 0; i < listaPvsAlcancados.size(); i++)
+  {
+    int teste = listaPvsAlcancados.at(i);
     cout << ' ' << teste;
   }
+
   system("pause");
   return 0;
 }
